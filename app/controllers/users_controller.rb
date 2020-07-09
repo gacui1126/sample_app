53d0df9f8933b55b7a1ed46bdfb7e31cfbcef650
gacui1126 <gacui1126@gmail.com>
2020-07-09 10:04:42 +0700

    complete chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,28 @@
 class UsersController < ApplicationController
   def show
-    # @user = User.find(params[:id])
-    # debugger
+
   end
 
   def new
-    # @user = User.new
+    @user = User.find_by(params[:id])
   end
-  # def create
-  # @user = User.new(user_params)
-  # if @user.save
-  # # Handle a successful save.
-  # else render 'new'
-  #   end
-  # end
-  # private
-  # def user_params
-  #   # params.require(:user).permit(:name,:email,:password,:password_confirmation)
-  # end
+
+  def new
+    @user = User.new
+  end
+  def create
+  @user = User.new(user_params)
+  if @user.save
+    flash[:success] = "wellcom_Hero!"
+    redirect_to user_url(@user)
+  # Handle a successful save.
+  else render :new
+    end
+  end
+  private
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,8 @@ Rails.application.routes.draw do
   get '/about', to:'static_pages#about'
   get '/contact', to:'static_pages#contact'
   get '/signup', to: 'users#new'
-  # resources :users
+
+  resources :users
   #resources:microposts
   #resources:users
   #root'users#index'
